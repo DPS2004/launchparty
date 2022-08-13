13c138e3eac5868ff748779155362707756c50d2
DPS2004 <10176105+DPS2004@users.noreply.github.com>
2022-08-13 00:26:26 -0400

    clean up code a bit and add a number

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,69 @@
 #include <3ds.h>
 #include <stdio.h>
 #include <cstring>
+#include <string>
+
+
+bool doQuit = false;
+
+int coolInt = 0;
+
+void pr(std::string t, int x, int y)
+{
+	std::string printstr = "\x1b[" + std::to_string(y + 1) + ";" + std::to_string(x).c_str() + "H" + t;
+	printf(printstr.c_str() );
+	
+}
+
+void update()
+{
+	hidScanInput();
+
+	u32 kDown = hidKeysDown();
+	
+	if (kDown & KEY_LEFT)
+	{
+		coolInt -= 1;
+	}
+	if (kDown & KEY_RIGHT)
+	{
+		coolInt += 1;
+	}
+	
+	if (kDown & KEY_START)
+	{	
+		//based on ModMoon implementation
+		u8 param[0x300];
+		u8 hmac[0x20];
+		u64 mm_id = 0x000400000018A400;
+		
+		memset(param, 0, sizeof(param));
+		memset(hmac, 0, sizeof(hmac));
+		
+		APT_PrepareToDoApplicationJump(0, mm_id, 1); //assumes USA copy stored on SD
+		APT_DoApplicationJump(param, sizeof(param), hmac);
+	}
+	
+	if (kDown & KEY_SELECT)
+	{
+		doQuit = true;
+	}
+}
+
+
+void draw()
+{
+	consoleClear();
+	pr("LaunchParty!",0,1);
+	pr("Press START to launch RHMM",0,3);
+	pr("Press SELECT to quit",0,4);
+	
+	pr(std::to_string(coolInt),1,6);
+	
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	gspWaitForVBlank();
+}
 
 
 int main(int argc, char **argv)
@@ -11,43 +74,19 @@ int main(int argc, char **argv)
 	//use console for now, eventually probably use citro2d?
 	consoleInit(GFX_TOP, NULL);
 	
-	printf("\x1b[2;0HLaunchParty!");
-	printf("\x1b[4;0HPress START to launch RHMM");
-	printf("\x1b[5;0HPress SELECT to quit");
+	
 	
 	while (aptMainLoop())
 	{
+		update();
 		
-		hidScanInput();
-
-		u32 kDown = hidKeysDown();
+		draw();
 		
-		
-		if (kDown & KEY_START)
-		{	
-			//based on ModMoon implementation
-			u8 param[0x300];
-			u8 hmac[0x20];
-			u64 mm_id = 0x000400000018A400;
-			
-			memset(param, 0, sizeof(param));
-			memset(hmac, 0, sizeof(hmac));
-			
-			APT_PrepareToDoApplicationJump(0, mm_id, 1); //assumes USA copy stored on SD
-			APT_DoApplicationJump(param, sizeof(param), hmac);
-		}
-		
-		if (kDown & KEY_SELECT)
+		if(doQuit)
 		{
-			break; // break in order to return to hbmenu
+			break;
 		}
 		
-		
-		
-		
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-		gspWaitForVBlank();
 	}
 	
 	
